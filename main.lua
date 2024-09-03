12b9746f831bb007098490dcc292938ffd76f50d
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-09-03 09:50:03 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,23 +16,12 @@ mods.on_all_mods_loaded(function()
     for _, m in pairs(mods) do
         if type(m) == "table" and m.RoRR_Modding_Toolkit then
             Callback = m.Callback
-            Instance = m.Instance
             Player = m.Player
             Resources = m.Resources
             break
         end
     end
 end)
-
-local players = nil
-local skulls_sprite = nil
-local skull_colours = {
-    gm.make_colour_rgb(56, 142, 240),
-    gm.make_colour_rgb(63, 197, 227),
-    gm.make_colour_rgb(94, 218, 246),
-    gm.make_colour_rgb(191, 237, 248),
-    gm.make_colour_rgb(255, 255, 255)
-}
 
 -- ========== ImGui ==========
 
@@ -44,43 +33,41 @@ gui.add_to_menu_bar(function()
     end
 end)
 
--- ========== Main ==========
+-- ========== Sprites ==========
+
+local skulls_sprite = nil
+local skull_colours = {
+    gm.make_colour_rgb(56, 142, 240),
+    gm.make_colour_rgb(63, 197, 227),
+    gm.make_colour_rgb(94, 218, 246),
+    gm.make_colour_rgb(191, 237, 248),
+    gm.make_colour_rgb(255, 255, 255)
+}
+
+-- ========== Init/Callback ==========
+
+local player = nil
+local getPlayers = function()
+    player = Player.get_client()
+end
 
 __initialize = function()
     skulls_sprite = Resources.sprite_load(path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "skulls.png"), 5, false, false, 13, 11)
-end
+    local banditSkull = gm.variable_global_get("class_buff")[37]
+    gm.array_set(banditSkull, 3, skulls_sprite)
+    gm.array_set(banditSkull, 9, -1)
 
-gm.post_code_execute(function(self, other, code, result, flags)
-    if code.name:match("oInit_Draw_7") then
-
-        players = Instance.find_all(gm.constants.oP)
-
-        if not players then return end
-
-        for _, player in ipairs(players) do
-            if not player then return end
-            if not params['IBS_enabled'] or player.buff_stack[37] == 0 then return end
-
+    Callback.add("onStageStart", "InifiniteBanditStandoff-getPlayers", getPlayers)
+    
+    gm.post_code_execute(function(self, other, code, result, flags)
+        if code.name:match("oInit_Draw_7") and params['IBS_enabled'] then
+            if not player or not player.buff_stack or player.buff_stack[37] == 0 then return end
+    
             local colour = skull_colours[gm.clamp(player.buff_stack[37], 1, 5)]
-
+    
             gm.draw_set_font(25)
             gm.draw_set_halign(0)
             gm.draw_text_colour(player.x + 8, player.y - 42, player.buff_stack[37], colour, colour, colour, colour, 1)
         end
-    end
-end)
-
-local hooks = {}
-hooks["gml_Object_oStartMenu_Step_2"] = function() -- mod init
-    hooks["gml_Object_oStartMenu_Step_2"] = nil
-
-    local banditSkull = gm.variable_global_get("class_buff")[37]
-    banditSkull[4] = skulls_sprite
-    banditSkull[10] = -1
+    end)
 end
-
-gm.post_code_execute(function(self, other, code, result, flags)
-    if hooks[code.name] then
-        hooks[code.name](self)
-    end
-end)
